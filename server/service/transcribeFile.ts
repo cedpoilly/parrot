03c2ab295f28c5590d13c38e73e2323c0beb4191
Cedric <cedpoilly@gmail.com>
2023-07-28 15:18:52 +0400

    fix: Use the OpenAI package, remove Whisper class

--- server/service/transcribeFile.ts
+++ server/service/transcribeFile.ts
@@ -1,17 +1,32 @@
-import { PathLike } from "fs"
+import { createReadStream, PathLike } from "fs"
 
-import Whisper from "../Whisper"
-const whisper = new Whisper(process.env.OPENAI_KEY as string)
+import { Configuration, OpenAIApi } from "openai"
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_KEY,
+})
+
+const openai = new OpenAIApi(configuration)
 
 export async function transcribeFile(path: PathLike) {
   let response
   let error
 
   try {
-    response = await whisper.transcribe(path, "whisper-1")
+    const transcription = await openai.createTranscription(
+      // * Casting the stream as `File` as the definition seems wrong (or does work with a stream)
+      createReadStream(path) as unknown as File, // The audio file to transcribe.
+      "whisper-1", // The model to use for transcription.
+      undefined, // The prompt to use for transcription.
+      "json", // The format of the transcription.
+      1, // Temperature
+      "en", // Language
+    )
+
+    response = transcription.data.text
   } catch (caughtError) {
     error = caughtError
-    console.error(caughtError)
+    console.error("caughtError:", caughtError)
   }
 
   return { response, error }
