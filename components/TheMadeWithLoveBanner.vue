68beafaf3e5ee2d7defe4fee1d39c8c27a6838f0
Cedric <cedpoilly@gmail.com>
2023-07-29 05:19:33 +0400

    style: fix footer

--- components/TheMadeWithLoveBanner.vue
+++ components/TheMadeWithLoveBanner.vue
@@ -1,15 +1,20 @@
 <template>
-  <div class="relative h-6 mt-5">
-    <p class="absolute z-10 w-full h-full text-center text-micro border-none py-1 text-slate-500">
+  <div class="relative mt-5">
+    <p
+      class="w-full h-full text-center text-micro border-none py-1 text-slate-500"
+    >
       made with ❤️ by
       <a
         class="link no-underline"
         href="https://github.com/cedpoilly"
         target="_blank"
-      >@cedpoilly</a>
+        >@cedpoilly</a
+      >
     </p>
     <div
       class="absolute w-full background bg-sky-100 text-sky-100 dark:bg-slate-800 dark:text-slate-800"
-    >.</div>
+    >
+      .
+    </div>
   </div>
 </template>
